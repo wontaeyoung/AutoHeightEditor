70ec958788827dd12c4faaba286c1e2bd145d30d
Taeyoung Won <hexter1994@gmail.com>
2023-11-05 19:43:32 +0900

    [Build] Project Init

---
+++ AutoHeightEditor/AutoHeightEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoHeightEditorApp.swift
+//  AutoHeightEditor
+//
+//  Created by 원태영 on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AutoHeightEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoHeightEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutoHeightEditor
+//
+//  Created by 원태영 on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
