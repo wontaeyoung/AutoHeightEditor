88a1b91c2c7f2a369a3f5aee4ec8253e312fed63
Taeyoung Won <hexter1994@gmail.com>
2023-11-06 23:43:54 +0900

    [Feat] 익스텐션 UIFont 추가 - Font -> UIFont 매핑 함수 추가

--- AutoHeightEditor/Extension.swift
+++ AutoHeightEditor/Extension.swift
@@ -16,3 +16,27 @@ extension Int {
         return CGFloat(self)
     }
 }
+
+extension UIFont {
+    // MARK: Method - SwiftUI system Font를 UIFont 타입으로 변환하는 함수
+    static func fontToUIFont(from font: Font) -> UIFont {
+        let style: UIFont.TextStyle
+        
+        switch font {
+        case .largeTitle:  style = .largeTitle
+        case .title:       style = .title1
+        case .title2:      style = .title2
+        case .title3:      style = .title3
+        case .headline:    style = .headline
+        case .subheadline: style = .subheadline
+        case .callout:     style = .callout
+        case .caption:     style = .caption1
+        case .caption2:    style = .caption2
+        case .footnote:    style = .footnote
+        case .body:        style = .body
+        default:           style = .body
+        }
+        
+        return  UIFont.preferredFont(forTextStyle: style)
+    }
+}
