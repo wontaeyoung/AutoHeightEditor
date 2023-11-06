fa37c8e6f9676d0c38b81a0f33b92af851808674
Taeyoung Won <hexter1994@gmail.com>
2023-11-06 23:47:53 +0900

    [Add] TextEditorConst 상수 열거형 추가

---
+++ AutoHeightEditor/Constant.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum TextEditorConst {
+    static let TEXTEDITOR_DEFAULT_LINE_COUNT: Int = 1
+    static let TEXTEDITOR_MAX_LINE_COUNT: Int = 5
+    static let TEXTEDITOR_INSET_HORIZONTAL: CGFloat = 10
+    static let TEXTEDITOR_WIDTH_HORIZONTAL_BUFFER: CGFloat = 12
+    static let TEXTEDITOR_INSET_BOTTOM: CGFloat = -3
+    static let TEXTEDITOR_STROKE_CORNER_RADIUS: CGFloat = 20
+    static let TEXTEDITOR_FRAME_HEIGHT_FREESPACE: CGFloat = 20
+}
