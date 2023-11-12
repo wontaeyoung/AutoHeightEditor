eb14a1cb0ddd435e378e2380bb97318ba769611f
Taeyoung Won <hexter1994@gmail.com>
2023-11-12 22:57:43 +0900

    [Build] AutoHeightEditor 소스코드 추가

---
+++ Sources/AutoHeightEditor/Constant.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum TextEditorConst {
+    static let TEXTEDITOR_DEFAULT_LINE_COUNT: Int = 1
+    static let TEXTEDITOR_MAX_LINE_COUNT: Int = 5
+    static let TEXTEDITOR_INSET_HORIZONTAL: CGFloat = 10
+    static let TEXTEDITOR_WIDTH_HORIZONTAL_BUFFER: CGFloat = 17
+    static let TEXTEDITOR_INSET_BOTTOM: CGFloat = 0
+    static let TEXTEDITOR_STROKE_CORNER_RADIUS: CGFloat = 20
+    static let TEXTEDITOR_FRAME_HEIGHT_FREESPACE: CGFloat = 20
+}
