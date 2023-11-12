39b11331752aab81257253a405a6369ef7c51da4
Taeyoung Won <hexter1994@gmail.com>
2023-11-13 00:40:03 +0900

    [Build] 라이브러리 소스코드 최소 iOS 지원 버전 14+ 명시

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AutoHeightEditor",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
